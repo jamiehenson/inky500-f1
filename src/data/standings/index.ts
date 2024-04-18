b683aca9ac9dd7d66df13b3e8b582d491beb95e5
Jamie Henson <jamie@jamiehenson.com>
2024-04-18 22:35:03 +0100

    chore: remove unneeded data files

--- src/data/standings/index.ts
+++ src/data/standings/index.ts
@@ -1,6 +1,5 @@
-import s1 from './s1.json'
 import s2 from './s2.json'
 import s3 from './s3.json'
 import s4 from './s4.json'
 
-export default { s1, s2, s3, s4 }
+export default { s1: {}, s2, s3, s4 }
